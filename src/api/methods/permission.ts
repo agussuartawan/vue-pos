6bafda72d0d374098e27e0fde372d7f3f78a5c22
agus <agus@dimata.com>
2024-11-12 10:06:47 +0800

    fix cors

--- src/api/methods/permission.ts
+++ src/api/methods/permission.ts
@@ -1,6 +1,6 @@
-import { axiosInstance } from '@/api/axios_instance'
 import type { PermissionResponse } from '@/api/data/response/permission_response'
 import type { BaseResponse } from '@/api/data/response/response'
+import { axiosInstance } from '@/api/axios_instance'
 
 export const getPermission = async () =>
-  await axiosInstance.get<BaseResponse<PermissionResponse[]>>('/permission')
+  await axiosInstance.get<BaseResponse<PermissionResponse[]>>('permission')
